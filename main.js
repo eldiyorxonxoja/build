4574f2390a3f317f29b9e1931832307cd3dc383e
eldiyorxonxoja <eldiyorxonxojaumarov3@gmail.com>
2021-03-27 09:14:09 +0500

    fayl

---
+++ main.js
@@ -0,0 +1,21 @@
+//selection element Function
+const selectElement = function (element){
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('open');
+});
+
+// Scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal(' .animate-left ', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300,
+})
